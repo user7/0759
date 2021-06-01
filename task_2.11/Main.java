17834412d32e5ee50e2a7b08dcf39e583b8a301d
svr <svr.work@protonmail.com>
2021-06-01 13:35:31 +0300

    task 2.11

--- task_2.11/Main.java
+++ task_2.11/Main.java
@@ -4,7 +4,23 @@
 
 public class Main {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        countpast10(new int[] {});
+        countpast10(new int[] {1, 2});
+        countpast10(new int[] {1, 2, 3, 4});
+        countpast10(new int[] {10, -1, 2});
+        countpast10(new int[] {10, 1, 2, 5, 6});
+        countpast10(new int[] {111, 3, 3, 3});
+    }
 
+    public static void countpast10(int a[]) {
+        int sum = 0;
+        for (int i = 0; i < a.length; ++i) {
+            sum += a[i];
+            if (sum > 10) {
+                System.out.format("сложить %d\n", i + 1);
+                return;
+            }
+        }
+        System.out.println("невозможно получить сумму больше 10");
     }
 }
