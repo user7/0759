c72fccf1cd4340b4028350407541086658706d8b
svr <svr.work@protonmail.com>
2021-06-01 13:26:37 +0300

    task 2.10

--- task_2.10/Main.java
+++ task_2.10/Main.java
@@ -4,7 +4,21 @@
 
 public class Main {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        checkseq(new int[] {1, 2, 3, 3, 5});
+        checkseq(new int[] {1, 2, 3, 2, 2});
+        checkseq(new int[] {1, 1});
+        checkseq(new int[] {1, 5, 1, 5, 1});
+        checkseq(new int[] {9});
+        checkseq(new int[] {});
+    }
 
+    public static void checkseq(int a[]) {
+        for (int i = 0; i + 1 < a.length; ++i) {
+            if (a[i] == a[i + 1]) {
+                System.out.println("да");
+                return;
+            }
+        }
+        System.out.println("нет");
     }
 }
