b4f20008f4b878bd369de1e5dc14a9682d1e5cd4
svr <svr.work@protonmail.com>
2021-06-01 13:15:41 +0300

    task 2.8

--- task_2.8/Main.java
+++ task_2.8/Main.java
@@ -4,7 +4,11 @@
 
 public class Main {
     public static void main(String[] args) {
-        //напишите тут ваш код
-
+        int a[] = new int[] {2, 3, 4, 5};
+        int p = 1;
+        for (int i = 0; i < a.length; ++i) {
+            p *= a[i];
+        }
+        System.out.println(p);
     }
 }
