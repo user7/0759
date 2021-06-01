8add89471cafeb3c404b5d05308563ea640152d8
svr <svr.work@protonmail.com>
2021-06-01 12:41:34 +0300

    task 2.4

--- task_2.4/Main.java
+++ task_2.4/Main.java
@@ -2,9 +2,15 @@
 Задание: Пользователь вводит сумму вклада и процент, который будет начисляться ежегодно. Отобразить размер вклада поочередно на ближайшие 5 лет.
 */
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //напишите тут ваш код
-
+        Scanner s = new Scanner(System.in);
+        int summ = s.nextInt(), percent = s.nextInt();
+        for (int i = 0; i < 5; ++i) {
+            summ = summ * (100 + percent) / 100;
+            System.out.format("Через %d год(а): %d\n", i + 1, summ);
+        }
     }
 }
