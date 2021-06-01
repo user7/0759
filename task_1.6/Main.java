76860d9062abb3adda35e7ffec6404a5f7d7143b
svr <svr.work@protonmail.com>
2021-06-01 11:35:18 +0300

    task 1.6

--- task_1.6/Main.java
+++ task_1.6/Main.java
@@ -2,8 +2,12 @@
 Пользователь вводит число, необходимо увеличить это число на 15% и вывести результат на экран
 */
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Ваш код
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        System.out.println(a * 115 / 100);
     }
 }
