dd4581096a157e7156f768af032f8f86b264c4a1
svr <svr.work@protonmail.com>
2021-06-01 12:45:18 +0300

    task 2.5

--- task_2.5/Main.java
+++ task_2.5/Main.java
@@ -2,9 +2,14 @@
 Задание: Запросить у пользователя ввод числа и сохранить это число в переменную a.Если переменная a равна 10, то выведите 'Верно', иначе выведите 'Неверно'.
 */
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //напишите тут ваш код
-
+        System.out.print("Угадайте число: ");
+        if (new Scanner(System.in).nextInt() == 10)
+            System.out.println("Верно");
+        else
+            System.out.println("Неверно");
     }
 }
