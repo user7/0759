de383139f7ef70b4254a66c68c57b393bdfa0c0e
svr <svr.work@protonmail.com>
2021-06-01 13:21:57 +0300

    task 2.9

--- task_2.9/Main.java
+++ task_2.9/Main.java
@@ -4,7 +4,18 @@
 
 public class Main {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        check5(new int[] {1, 2, 3, 4, 5});
+        check5(new int[] {1, 2, 3, 4});
+        check5(new int[] {1, 5, 3});
+    }
 
+    public static void check5(int a[]) {
+        for (int i = 0; i < a.length; ++i) {
+            if (a[i] == 5) {
+                System.out.println("да");
+                return;
+            }
+        }
+        System.out.println("нет");
     }
 }
