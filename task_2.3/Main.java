e19d4ed246b9598fb6f4d851aef35dd7f612c540
svr <svr.work@protonmail.com>
2021-06-01 12:33:15 +0300

    task 2.3

--- task_2.3/Main.java
+++ task_2.3/Main.java
@@ -15,7 +15,11 @@
 
 public class Main {
     public static void main(String[] args) {
-        //напишите тут ваш код
-
+        for (int i = 0; i < 10; ++i) {
+            for (int j = 0; j <= i; ++j) {
+                System.out.print("8");
+            }
+            System.out.println();
+        }
     }
 }
