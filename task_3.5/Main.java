e1308ad69502cf83472a440913523f0149763809
svr <svr.work@protonmail.com>
2021-06-01 14:23:24 +0300

    task 3.5

--- task_3.5/Main.java
+++ task_3.5/Main.java
@@ -5,6 +5,14 @@
 
 public class Main {
     public static void main(String[] args) {
-        int num[] = {4,2,6,2,65}
+        int num[] = {4,2,6,2,65};
+        printRec(num, 0);
+    }
+
+    public static void printRec(int arr[], int i) {
+        if (i < arr.length) {
+            System.out.println(arr[i]);
+            printRec(arr, i + 1);
+        }
     }
 }
