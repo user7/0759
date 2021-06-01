0dbd35258189089b5bca88a2c6a933cd7a69711a
svr <svr.work@protonmail.com>
2021-06-01 12:49:45 +0300

    task 2.6

--- task_2.6/Main.java
+++ task_2.6/Main.java
@@ -4,7 +4,15 @@
 
 public class Main {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        divcheck(9, 3);
+        divcheck(9, 5);
+    }
 
+    public static void divcheck(int a, int b) {
+        int r = a % b;
+        if (r == 0)
+            System.out.println("Делится");
+        else
+            System.out.format("Делится с остатком %d\n", r);
     }
 }
