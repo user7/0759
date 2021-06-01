e519316ecb1234fca524140e799f8f3a48283c63
svr <svr.work@protonmail.com>
2021-06-01 15:47:51 +0300

    task 5.1

--- task_5.1/Main.java
+++ task_5.1/Main.java
@@ -5,17 +5,31 @@
 * */
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<String> names = new ArrayList();
+        ArrayList<String> names = new ArrayList<String>();
         names.add("Анатолий");
         names.add("Георгий");
         names.add("Руслан");
         names.add("Георгий");
         names.add("Павел");
         names.add("Руслан");
+
+        HashSet<String> set = new HashSet<String>();
+        ArrayList<String> names2 = new ArrayList<String>();
+        Iterator<String> iter = names.iterator();
+        while (iter.hasNext()) {
+            String name = iter.next();
+            if (!set.contains(name)) {
+                names2.add(name);
+                set.add(name);
+            }
+        }
+        names = names2;
+        System.out.println(names);
     }
 }
