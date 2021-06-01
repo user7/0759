c209c76536a2525ee03e5ec3e2f961dbd995b849
svr <svr.work@protonmail.com>
2021-06-01 14:43:44 +0300

    task 4.4

--- task_4.4/Main.java
+++ task_4.4/Main.java
@@ -19,9 +19,11 @@ Mouse jerryMouse = new Mouse("Jerry", 12 , 5),
 
 public class Main {
     public static void main(String[] args) {
-        Mouse jerryMouse = new Mouse("Jerry", 12, 5);
-
-        //напишите тут ваш код
+        Mouse m1 = new Mouse("Jerry", 12, 5);
+        Mouse m2 = new Mouse("мышиха", 12, 5);
+        Cat c1 = new Cat("Том", 12, 5);
+        Cat c2 = new Cat("кошка", 12, 5);
+        Dog d = new Dog("бульдог", 12, 5);
     }
 }
 class Mouse {
@@ -30,6 +32,28 @@ class Mouse {
     int tail;
 
     public Mouse(String name, int height, int tail) {
+        this.name = name;
+        this.height = height;
+        this.tail = tail;
+    }
+}
+class Dog {
+    String name;
+    int height;
+    int tail;
+
+    public Dog(String name, int height, int tail) {
+        this.name = name;
+        this.height = height;
+        this.tail = tail;
+    }
+}
+class Cat {
+    String name;
+    int height;
+    int tail;
+
+    public Cat(String name, int height, int tail) {
         this.name = name;
         this.height = height;
         this.tail = tail;
