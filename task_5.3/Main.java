f07c4fcbc029e9796e098fa471f0247f002c0f03
svr <svr.work@protonmail.com>
2021-06-02 08:42:03 +0300

    task 5.3

--- task_5.3/Main.java
+++ task_5.3/Main.java
@@ -4,8 +4,14 @@
 */
 
 import java.util.ArrayList;
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-    
+        ArrayList<Integer> a = new ArrayList<Integer>(Arrays.asList(1, 4, -9, 12, -1, 0, 10, 7));
+        ArrayList<Integer> b = new ArrayList<Integer>();
+        for (int i = 0; i < a.size(); i++)
+            if (a.get(i) > 0 && a.get(i) < 10)
+                b.add(a.get(i));
+        System.out.println(b);
     }
 }
