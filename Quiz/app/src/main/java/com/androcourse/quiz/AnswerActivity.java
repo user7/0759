9ff4416f393b14a95def994839c7b6fcaf150744
svr <svr@protonmail.com>
2021-06-29 13:41:07 +0300

    Quiz wip

---
+++ Quiz/app/src/main/java/com/androcourse/quiz/AnswerActivity.java
@@ -0,0 +1,28 @@
+package com.androcourse.quiz;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class AnswerActivity extends AppCompatActivity {
+    private TextView answerTextView;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_answer);
+
+        TextView caption = findViewById(R.id.answerCaptionTextView);
+        caption.setText(R.string.answersCaptionLabel);
+
+        answerTextView = findViewById(R.id.answerTextView);
+        String[] answers = getIntent().getStringArrayExtra("answers");
+        int answersGood = getIntent().getIntExtra("answersGood", 0);
+        String text = String.join("\n", answers) + "\n\n" + "Верно: " + answersGood + " из " + answers.length;
+        answerTextView.setText(text);
+    }
+}
--- Quiz/app/src/main/java/com/androcourse/quiz/MainActivity.java
+++ Quiz/app/src/main/java/com/androcourse/quiz/MainActivity.java
@@ -1,8 +1,8 @@
 package com.androcourse.quiz;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -10,13 +10,14 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.w3c.dom.Text;
-
 public class MainActivity extends AppCompatActivity {
-    private Button yesBtn;
-    private Button noBtn;
-    private TextView questionTextView;
-    private int currentQuestion = 0;
+    Button yesButton;
+    Button noButton;
+    Button showAnswer;
+    Toast lastToast;
+    TextView questionTextView;
+    int currentQuestion = 0;
+    boolean needReset = true;
 
     Question[] questions = {
             new Question(R.string.question, false),
@@ -25,7 +26,46 @@ public class MainActivity extends AppCompatActivity {
             new Question(R.string.question3, false),
             new Question(R.string.question4, false),
     };
-    private Bundle savedInstanceState;
+    Bundle savedInstanceState;
+    String[] answers;
+    int answersGood = 0;
+
+    void
+    respond(boolean givenYes) {
+        Question q = questions[currentQuestion];
+        int message = R.string.messageIncorrect;
+        int answer = givenYes ? R.string.answerYes : R.string.answerNo;
+        int answerGoodness = R.string.answerIncorrect;
+        if (q.isAnswer() == givenYes) {
+            answersGood++;
+            message = R.string.messageCorrect;
+            answerGoodness = R.string.answerCorrect;
+        }
+        answers[currentQuestion] = getResources().getString(q.getQuestion()) +
+                getResources().getString(R.string.joinString) +
+                getResources().getString(answer) +
+                getResources().getString(R.string.joinString) +
+                getResources().getString(answerGoodness);
+
+        if (lastToast != null) {
+            lastToast.cancel();
+            lastToast = null;
+        }
+        if (currentQuestion + 1 < questions.length) {
+            needReset = false;
+            currentQuestion++;
+            Toast toast = Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT);
+            lastToast = toast;
+            toast.show();
+            updateOnscreenQuestion();
+        } else {
+            needReset = true;
+            Intent intent = new Intent(MainActivity.this, AnswerActivity.class);
+            intent.putExtra("answers", answers);
+            intent.putExtra("answersGood", answersGood);
+            startActivity(intent);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,29 +73,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState != null) {
             currentQuestion = savedInstanceState.getInt("qn");
-            Log.d("===", "onCreate non-null " + currentQuestion);
-        } else {
-            Log.d("===", "onCreate null");
         }
 
-        questionTextView = findViewById(R.id.questionTextView);
-        updateText();
+        answers = new String[questions.length];
 
-        yesBtn = findViewById(R.id.yesBtn);
-        yesBtn.setOnClickListener(new View.OnClickListener() {
+        questionTextView = findViewById(R.id.questionTextView);
+        updateOnscreenQuestion();
+
+        yesButton = findViewById(R.id.yesButton);
+        yesButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, produceResponse(true), Toast.LENGTH_SHORT).show();
-                nextQuestion();
+                respond(true);
             }
         });
 
-        noBtn = findViewById(R.id.noBtn);
-        noBtn.setOnClickListener(new View.OnClickListener() {
+        noButton = findViewById(R.id.noButton);
+        noButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, produceResponse(false), Toast.LENGTH_SHORT).show();
-                nextQuestion();
+                respond(false);
             }
         });
     }
@@ -73,22 +110,8 @@ public class MainActivity extends AppCompatActivity {
         savedInstanceState.putInt("qn", currentQuestion);
     }
 
-    void nextQuestion() {
-        rollQuestion(1);
-        updateText();
-    }
-
-    int produceResponse(boolean givenYes) {
-        return questions[currentQuestion].isAnswer() == givenYes ? R.string.correct : R.string.incorrect;
-    }
-
-    void rollQuestion(int delta) {
-        assert (questions.length > 0);
-        currentQuestion = (currentQuestion + delta) % questions.length;
-    }
-
-    void updateText() {
-        rollQuestion(0);
+    void updateOnscreenQuestion() {
+        assert (currentQuestion < questions.length);
         questionTextView.setText(questions[currentQuestion].getQuestion());
     }
 }
